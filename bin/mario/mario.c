a63b6c837abe0e269086186caa21a4a8b46a5c2e
misazhu <misa.zhu@gmail.com>
2026-02-24 16:28:00 +0800

    mario demos

--- bin/mario/mario.c
+++ bin/mario/mario.c
@@ -26,7 +26,7 @@ void mario_mem_quit(void) {
 }
 
 static void out(const char* str) {
-    write(1, str, strlen(str));
+    printf("%s", str);
 }
 
 void platform_init(void) {
@@ -50,11 +50,10 @@ void init_args(vm_t* vm, int argc, char** argv) {
 	var_t* args = var_new_array(vm);
 	int i;
 	for(i=0; i<argc; i++) {
+		printf("args[%d] = %s\n", i, argv[i]);
 		var_t* v = var_new_str(vm, argv[i]);
 		var_array_add(args, v);
-		var_unref(v);
 	}
-
 	var_add(vm->root, "_args", args);
 }
 
@@ -100,82 +99,63 @@ static int doargs(int argc, char* argv[]) {
 	return 0;
 }
 
-static inline var_t* vm_load_var(vm_t* vm, const char* name, bool create) {
-	node_t* n = vm_load_node(vm, name, create);
-	if(n != NULL)
-		return n->var;
-	return NULL;
-}
-
-static inline void vm_load_basic_classes(vm_t* vm) {
-	vm->builtin_vars.var_Object = vm_load_var(vm, "Object", false);
-	vm->builtin_vars.var_String = vm_load_var(vm, "String", false);
-	vm->builtin_vars.var_Number = vm_load_var(vm, "Number", false);
-	vm->builtin_vars.var_Error = vm_load_var(vm, "Error", false);
-	vm->builtin_vars.var_Array = vm_load_var(vm, "Array", false);
-}
-
 bool js_compile(bytecode_t *bc, const char* input);
 
 int main(int argc, char** argv) {
-	setbuf(stdin, NULL);
-	setbuf(stdout, NULL);
-
-	platform_init();
-
 	if(doargs(argc, argv) != 0) {
 		printf("Usage: mario (-c/d/a) <filename>\n");
 		return -1;
 	}
 
+	platform_init();
 	mario_mem_init();
-	bool loaded = true;
 
 	vm_t* vm = vm_new(js_compile, VAR_CACHE_MAX_DEF, LOAD_NCACHE_MAX_DEF);
-	vm->gc.gc_trig_var_num = 1024;
+	if(vm == NULL) {
+		printf("Failed to create VM. make sure all platform functions(_platform_malloc, _platform_free, _platform_out) are set.\n");
+		mario_mem_quit();
+		return -1;
+	}
+	vm_init(vm, reg_natives, NULL);
+
 	init_args(vm, argc, argv);
 
-	if(loaded) {
-		vm_init(vm, reg_natives, NULL);
-		vm_load_basic_classes(vm);
-
-		if(_fname[0] != 0) {
-			bool res = false;
-			if(strstr(_fname, ".js") != NULL)
-				res = load_js(vm, _fname);
-			else if(strstr(_fname, ".mbc") != NULL && _mode != 2) {
-				bc_release(&vm->bc);
-				res = vm_load_mbc(vm, _fname);
-			}
-			
-			if(res) {
-				if(_mode == MODE_CMPL) {
-					// 如果没有指定输出文件名，默认使用输入文件名（将 .js 改为 .mbc）
-					const char* output_name = _fname_out;
-					char auto_output[256] = {0};
-					if(output_name[0] == 0) {
-						const char* js_ext = strstr(_fname, ".js");
-						if(js_ext) {
-							// 复制输入文件名到 auto_output，替换 .js 为 .mbc
-							size_t ext_pos = js_ext - _fname;
-							strncpy(auto_output, _fname, ext_pos);
-							strcat(auto_output, ".mbc");
-							output_name = auto_output;
-						}
+	if(_fname[0] != 0) {
+		bool res = false;
+		if(strstr(_fname, ".js") != NULL)
+			res = load_js(vm, _fname);
+		else if(strstr(_fname, ".mbc") != NULL && _mode != 2) {
+			bc_release(&vm->bc);
+			res = vm_load_mbc(vm, _fname);
+		}
+		
+		if(res) {
+			if(_mode == MODE_CMPL) {
+				// If no output filename is specified, use the input filename by default (change .js to .mbc)
+				const char* output_name = _fname_out;
+				char auto_output[256] = {0};
+				if(output_name[0] == 0) {
+					const char* js_ext = strstr(_fname, ".js");
+					if(js_ext) {
+						// Copy input filename to auto_output, replace .js with .mbc
+						size_t ext_pos = js_ext - _fname;
+						strncpy(auto_output, _fname, ext_pos);
+						strcat(auto_output, ".mbc");
+						output_name = auto_output;
 					}
-					vm_gen_mbc(vm, output_name);
 				}
-				else {
-					if(_dump) {
-						mstr_t* dump = bc_dump(&vm->bc);
-						if(dump != NULL) {
-							_platform_out(dump->cstr);
-							mstr_free(dump);
-						}
+				vm_gen_mbc(vm, output_name);
+			}
+			else {
+				if(_dump) {
+					mstr_t* dump = bc_dump(&vm->bc);
+					if(dump != NULL) {
+						_platform_out(dump->cstr);
+						mstr_free(dump);
 					}
-					else  {
-						vm_run(vm);
-					}
+				}
+				else  {
+					vm_run(vm);
 				}
 			}
 		}
--- lang/js/native/natives.c
+++ lang/js/native/natives.c
@@ -1,6 +1,22 @@
 #include "mario.h"
 #include "native_builtin.h"
 
+static inline var_t* vm_load_var(vm_t* vm, const char* name, bool create) {
+	node_t* n = vm_load_node(vm, name, create);
+	if(n != NULL)
+		return n->var;
+	return NULL;
+}
+
+static inline void load_basic_classes(vm_t* vm) {
+	vm->builtin_vars.var_Object = vm_load_var(vm, "Object", false);
+	vm->builtin_vars.var_String = vm_load_var(vm, "String", false);
+	vm->builtin_vars.var_Number = vm_load_var(vm, "Number", false);
+	vm->builtin_vars.var_Error = vm_load_var(vm, "Error", false);
+	vm->builtin_vars.var_Array = vm_load_var(vm, "Array", false);
+}
+
 void reg_natives(vm_t* vm) {
 	reg_basic_natives(vm);
+	load_basic_classes(vm);
 }
